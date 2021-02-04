7ab50c01f828609978f63c00fedd248875c375cf
Stephan Schiffels <stephan_schiffels@mac.com>
2021-02-04 14:02:27 +0100

    changed path to bioinf directories

--- make-repo-directory.py
+++ make-repo-directory.py
@@ -4,10 +4,9 @@ import os
 import yaml
 import json
 
-dir_ = "/Users/schiffels/poseidon/repo"
-reldir = "/Users/schiffels/poseidon/repo"
-outJSON = "out.json"
-outMD = "out.md"
+dir_ = "/var/www/bioinf/htdocs/poseidon/repo"
+outJSON = "/var/www/bioinf/htdocs/poseidon/package_dir.json"
+outMD = "/var/www/bioinf/htdocs/poseidon/package_dir.md"
 
 repo_dir = []
 for path, dirs, files in os.walk(dir_):
@@ -16,8 +15,8 @@ for path, dirs, files in os.walk(dir_):
       with open(os.path.join(path, file_), 'r') as stream:
         yml = yaml.safe_load(stream)
       package_obj = {
-        'path' : os.path.relpath(path, start=reldir),
-        'pos_yaml' : os.path.relpath(os.path.join(path, file_), start=reldir),
+        'path' : os.path.relpath(path, start=dir_),
+        'pos_yaml' : os.path.relpath(os.path.join(path, file_), start=dir_),
         'title' : yml['title']
       }
       repo_dir.append(package_obj)
