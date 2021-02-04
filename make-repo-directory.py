6f2087de16dbb23e52b47b8e3350e936d3ba5b7d
Stephan Schiffels <stephan_schiffels@mac.com>
2021-02-04 22:43:17 +0100

    added automatic zipping

--- make-repo-directory.py
+++ make-repo-directory.py
@@ -3,30 +3,33 @@
 import os
 import yaml
 import json
+import sys
+import zipfile
 
 dir_ = "/Users/schiffels/poseidon/repo"
 reldir = "/Users/schiffels/poseidon/repo"
 outJSON = "out.json"
-outMD = "out.md"
 
 repo_dir = []
 for path, dirs, files in os.walk(dir_):
   for file_ in files:
     if file_ == "POSEIDON.yml":
+      print("Processing", path, file=sys.stderr)
+      zipFN = path + ".zip"
+      with zipfile.ZipFile(zipFN, 'w') as zipF:
+        for f in files:
+          zipF.write(os.path.join(path, f), arcname=f)
+
       with open(os.path.join(path, file_), 'r') as stream:
         yml = yaml.safe_load(stream)
       package_obj = {
         'path' : os.path.relpath(path, start=reldir),
         'pos_yaml' : os.path.relpath(os.path.join(path, file_), start=reldir),
-        'title' : yml['title']
+        'title' : yml['title'],
+        'zip_file' : zipFN
       }
       repo_dir.append(package_obj)
 
 with open(outJSON, 'w') as f:
   print(json.dumps(repo_dir), file=f)
 
-with open(outMD, 'w') as f:
-  print("| Package | URL |", file=f)
-  print("| ------- | --- |", file=f)
-  for package_obj in repo_dir:
-    print(f"| {package_obj['title']} | https://bioinf.eva.mpg.de/poseidon/repo/{package_obj['path']} |", file=f)
