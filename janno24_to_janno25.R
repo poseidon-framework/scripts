19899063a6cd8ab5f6305319d5ec52927b55ae4e
Clemens Schmid <clemens@nevrome.de>
2022-01-07 14:24:30 +0100

    further work on the script - should be able to run on many janno files now; also added column reordering

--- janno24_to_janno25.R
+++ janno24_to_janno25.R
@@ -1,41 +1,9 @@
 setwd("~/agora/published_data")
 
-jannoFile <- list.files(pattern = ".janno", recursive = T)[141]
-
-jannoDF <- readr::read_tsv(jannoFile)
-
-# simple column renaming
-jannoDFnewColNames <- jannoDF
-
-colNameLookup <- hash::hash(
-  c("Individual_ID", "No_of_Libraries", "Data_Type", "Nr_autosomal_SNPs", "Publication_Status", "Coverage_1240K"),
-  c("Poseidon_ID", "Nr_Libraries", "Capture_Type", "Nr_SNPs", "Publication", "Coverage_on_Target_SNPs")
-)
-
-replaceName <- function(x) {
-  if (x %in% hash::keys(colNameLookup)) {
-    hash::values(colNameLookup, x)
-  } else {
-    x
-  }
-}
-
-colnames(jannoDFnewColNames) <- sapply(colnames(jannoDFnewColNames), replaceName)
-
-# enable new contamination meas setup
-
-# add empty contam columns if missing
-if (!"Xcontam" %in% colnames(jannoDFnewColNames)) {
-  jannoDFnewColNames$Xcontam <- "n/a"
-  jannoDFnewColNames$Xcontam_stderr <- "n/a"
-}
-if (!"mtContam" %in% colnames(jannoDFnewColNames)) {
-  jannoDFnewColNames$mtContam <- "n/a"
-  jannoDFnewColNames$mtContam_stderr <- "n/a"
-}
+columnOrder <- readr::read_tsv("https://raw.githubusercontent.com/poseidon-framework/poseidon2-schema/janno_column_names_and_order/janno_columns.tsv")$janno_column_name
 
 isPoseidonNA <- function(x) {
-  all(x == "n/a" | x == "")
+  all(is.na(x) | x == "n/a" | x == "")
 }
 
 uniteWithoutNA <- function(x, y, toUniteFirst = x, toUniteSecond = y) {
@@ -54,18 +22,59 @@ uniteWithoutNA <- function(x, y, toUniteFirst = x, toUniteSecond = y) {
   )
 }
 
-contam <- jannoDFnewColNames |>
-  dplyr::select(Poseidon_ID, Xcontam, Xcontam_stderr, mtContam, mtContam_stderr) |>
-  dplyr::transmute(
-    Contamination = uniteWithoutNA(Xcontam, mtContam),
-    Contamination_Err = uniteWithoutNA(Xcontam, mtContam, Xcontam_stderr, mtContam_stderr),
-    Contamination_Meas = uniteWithoutNA(Xcontam, mtContam, "X-based (unknown software)", "mt-based (unknown software)")
-  )
+colNameLookup <- hash::hash(
+  c("Individual_ID", "No_of_Libraries", "Data_Type", "Nr_autosomal_SNPs", "Publication_Status", "Coverage_1240K"),
+  c("Poseidon_ID", "Nr_Libraries", "Capture_Type", "Nr_SNPs", "Publication", "Coverage_on_Target_SNPs")
+)
 
-jannoContam <- jannoDFnewColNames |>
-  dplyr::select(-Xcontam, -Xcontam_stderr, -mtContam, -mtContam_stderr) |>
-  dplyr::bind_cols(contam)
+replaceName <- function(x) {
+  if (x %in% hash::keys(colNameLookup)) {
+    hash::values(colNameLookup, x)
+  } else {
+    x
+  }
+}
 
-# remove columns with only empty values
-jannoDFwithoutNA <- jannoContam |>
-  dplyr::select(tidyselect:::where(\(x) {!isPoseidonNA(x)}))
+jannoFiles <- list.files(pattern = ".janno", recursive = T)[140:142]
+purrr::map(
+  jannoFiles, function(jannoFile) {
+    
+    jannoDF <- readr::read_tsv(jannoFile)
+    
+    # simple column renaming
+    jannoDFnewColNames <- jannoDF
+    colnames(jannoDFnewColNames) <- sapply(colnames(jannoDFnewColNames), replaceName)
+    
+    # enable new contamination meas setup
+    # add empty contam columns, if they are missing (technically more simple)
+    if (!"Xcontam" %in% colnames(jannoDFnewColNames)) {
+      jannoDFnewColNames$Xcontam <- "n/a"
+      jannoDFnewColNames$Xcontam_stderr <- "n/a"
+    }
+    if (!"mtContam" %in% colnames(jannoDFnewColNames)) {
+      jannoDFnewColNames$mtContam <- "n/a"
+      jannoDFnewColNames$mtContam_stderr <- "n/a"
+    }
+    
+    contam <- jannoDFnewColNames |>
+      dplyr::select(Poseidon_ID, Xcontam, Xcontam_stderr, mtContam, mtContam_stderr) |>
+      dplyr::transmute(
+        Contamination = uniteWithoutNA(Xcontam, mtContam),
+        Contamination_Err = uniteWithoutNA(Xcontam, mtContam, Xcontam_stderr, mtContam_stderr),
+        Contamination_Meas = uniteWithoutNA(Xcontam, mtContam, "X-based (unknown software)", "mt-based (unknown software)")
+      )
+    
+    jannoContam <- jannoDFnewColNames |>
+      dplyr::select(-Xcontam, -Xcontam_stderr, -mtContam, -mtContam_stderr) |>
+      dplyr::bind_cols(contam)
+    
+    # remove columns with only empty values
+    jannoDFwithoutNA <- jannoContam |>
+      dplyr::select(tidyselect:::where(\(x) {!isPoseidonNA(x)}))
+    
+    # reorder columns
+    jannoReordered <- jannoDFwithoutNA[columnOrder[columnOrder %in% colnames(jannoDFwithoutNA)]]
+    
+    return(list(jannoFile, jannoReordered))
+  }
+)
